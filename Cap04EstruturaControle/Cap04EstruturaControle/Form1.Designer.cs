d238e6972f3c40050721b8ef2c64d9170c6df8b6
Pedro Arns Junior <pedroarnsjr@outlook.com>
2024-06-24 16:08:40 -0300

    Capitulo 4 - Estrutura de Controles

---
+++ Cap04EstruturaControle/Cap04EstruturaControle/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Cap04EstruturaControle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Saque";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnSaque_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(59, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exercicio1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnExercicio1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(243, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exercicio2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnExercicio2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(243, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Exercicio3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnExercicio3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(160, 224);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Nota Fiscal";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btnValorDaNotaFiscal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 333);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Cap04EstruturaControle/Cap04EstruturaControle/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cap04EstruturaControle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaque_Click(object sender, EventArgs e)
+        {
+            double saldo = 100.0;
+            double valorSaque = 10.0;
+            if(saldo >= valorSaque)
+            {
+                saldo = saldo - valorSaque;
+                MessageBox.Show("Saque realizado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }     
+        }
+
+        private void btnExercicio1_Click(object sender, EventArgs e)
+        {
+            double saldo = 100.0;
+            double valorSaque = 10.0;
+            if(saldo >= valorSaque)
+            {
+                saldo -= valorSaque;
+                MessageBox.Show("Saque reslizado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }     
+        }
+
+        private void btnExercicio2_Click(object sender, EventArgs e)
+        {
+            double saldo = 5.0;
+            double valorSaque = 10.0;
+            if (saldo >= valorSaque)
+            {
+                saldo -= valorSaque;
+                MessageBox.Show("Saque reslizado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+        }
+
+        private void btnExercicio3_Click(object sender, EventArgs e)
+        {
+            double saldo = 500.0;
+            if(saldo < 0.0)
+            {
+                MessageBox.Show("Voce está no negativo");
+            }
+            else if(saldo > 1000000.0)
+            {
+                MessageBox.Show("Voce é um bom cliente");
+            }
+            else
+            {
+                MessageBox.Show("Voce é milionario");
+            }     
+        }
+
+        private void btnValorDaNotaFiscal_Click(object sender, EventArgs e)
+        {
+            double valorDaNotaFiscal = 5000.0;
+            double imposto = 0.0;
+
+            if(valorDaNotaFiscal <= 999)
+            {
+                imposto = valorDaNotaFiscal * 0.02;
+            }
+            else if(valorDaNotaFiscal >= 1000 && valorDaNotaFiscal <= 2999)
+            {
+                imposto = valorDaNotaFiscal * 0.028;
+            }
+            else if(valorDaNotaFiscal >= 3000 && valorDaNotaFiscal <= 6999)
+            {
+                imposto = valorDaNotaFiscal * 0.028;
+            }
+            else if(valorDaNotaFiscal >= 7000)
+            {
+                imposto = valorDaNotaFiscal * 0.03;
+            }
+
+            MessageBox.Show("O imposto calculado é: " + imposto.ToString("C"));
+        }
+    }
+}
