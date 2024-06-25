393d933efd290aa40ffbb6f9980fa7564dc43605
Pedro Arns Junior <pedroarnsjr@outlook.com>
2024-06-24 21:02:34 -0300

    Capitulo 05 Estruturas de Repetição

---
+++ Cap05EstruturaRepeticao/Cap05EstruturaRepeticao/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Cap05EstruturaRepeticao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Valor Investido For";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnValorInvestidoFor_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Repetindo Um Bloco de Código";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Valor Investido While";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnValorInvestidoWhile_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(237, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exercicio 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnExercicio1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(237, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Exercicio 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnExercicio2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 349);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Cap05EstruturaRepeticao/Cap05EstruturaRepeticao/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cap05EstruturaRepeticao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValorInvestidoFor_Click(object sender, EventArgs e)
+        {
+            double valorInvestido = 1000.0;
+            for(int i = 1; i <= 12; i += 1)
+            {
+                valorInvestido = valorInvestido * 1.01;
+            }
+            MessageBox.Show("Valor investido agora é " +valorInvestido);
+        }
+
+        private void btnValorInvestidoWhile_Click(object sender, EventArgs e)
+        {
+            double valorInvestido = 1000.0;
+            int i = 1;
+            while(i <= 12)
+            {
+                valorInvestido = valorInvestido * 1.01;
+                i += 1;
+            }
+            MessageBox.Show("Valor investido agora é " + valorInvestido);
+        }
+
+        private void btnExercicio1_Click(object sender, EventArgs e)
+        {
+            int total = 2;
+            for(int i = 0; i < 5; i += 1)
+            {
+                total = total * 2;
+            }
+            MessageBox.Show("O total é: " + total);
+        }
+
+        private void btnExercicio2_Click(object sender, EventArgs e)
+        {
+            int soma = 0;
+            for (int i = 1; i<= 1000; i++)
+            {
+                soma = soma + i;
+            }
+            MessageBox.Show("A soma dos numeros de 1 até 1000 é: " + soma.ToString());
+        }
+    }
+}
