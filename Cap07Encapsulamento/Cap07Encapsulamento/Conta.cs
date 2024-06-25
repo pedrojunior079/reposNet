b44db1b591ac2cb68a7fbee5983988750a8ab1b5
Pedro Arns Junior <pedroarnsjr@outlook.com>
2024-06-25 19:07:02 -0300

    Capitulo 7 Encapsulamento e Modificadores de Acesso

---
+++ Cap07Encapsulamento/Cap07Encapsulamento/Conta.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap07Encapsulamento
+{
+    internal class Conta
+    {
+        private int numero;
+        public int Numero
+        {
+            get
+            {
+                return this.numero;
+            }
+            set
+            {
+                this.numero = value;
+            }
+        }
+        private double saldo;
+        public double Saldo
+        {
+            get
+            {
+                return this.saldo;
+            }     
+            
+
+                
+            set
+            {
+                this.saldo = value;
+            }    
+        }
+
+        private Cliente titular;
+
+        public double PegaSaldo()
+        {
+            return this.saldo;
+        }
+
+        public void ColocaNumero(int numero)
+        {
+            this.numero = numero;
+        }     
+
+        public void Saca(double valor)
+        {
+            this.saldo -= valor;
+        }
+        
+        public void Deposita(double valor)
+        {
+            this.saldo += valor;
+        }     
+    }
+
+    class Cliente
+    {
+
+    }     
+}
---
+++ Cap07Encapsulamento/Cap07Encapsulamento/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cap07Encapsulamento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncapsulamento_Click(object sender, EventArgs e)
+        {
+            Conta conta = new Conta();
+            conta.ColocaNumero(1100);
+            MessageBox.Show("Saldo: " + conta.PegaSaldo());
+
+
+            Conta c = new Conta();
+            c.Numero = 1; // funciona pois o set do Numero é público
+            MessageBox.Show("Numero: " + c.Numero);
+
+            c.Saldo = 100.0; // set do saldo é privado, então temos um erro
+            MessageBox.Show("Saldo " + c.Saldo); // funciona pois o get do saldo é publico.
+            
+
+        }
+    }
+}
